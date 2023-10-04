87e47b63bbf91a05bc52919dde711044bf75f92b
achira-sriwattana <146061834+achira-sriwattana@users.noreply.github.com>
2023-10-04 10:28:40 +0700

    Cleaned display on quadratic_formula.c

--- quadratic_formula.c
+++ quadratic_formula.c
@@ -3,30 +3,71 @@
 #include <math.h>
 #include <stdbool.h>
 
+
 void quadratic_formula() {
     float a, b, c, d;
     float final1, final2;
 
     while (true) {
-        printf("\n\nEnter a, b, c (a,b,c): ");
+        printf("Enter a, b, c (a,b,c): ");
         scanf("%f,%f,%f", &a, &b, &c);
 
         getchar();
 
         // ax^2 + bx + c
 
+        switch((int)a){
+            case 0: printf(""); break;
+            case 1: printf("x^2"); break;
+            case -1: printf("-x^2"); break;
+            default: printf("%.1fx^2",a);
+        }
+
+        switch((int)b){
+            case 0: printf(""); break;
+            case 1: printf("+x"); break;
+            case -1: printf("-x"); break;
+            // default: printf("%.1fx", b);
+
+            default:
+                if ((b > 0) && (a != 0)){
+                    printf("+%.1fx", b);
+                }
+                else{
+                    printf("%.1fx", b);
+                }
+
+        }
+
+        switch((int)c){
+            case 0: printf(""); break;
+            // case 1: printf("%.1f", c): break;
+            case -1: printf("%.1f", c); break;
+            // default: printf("%.1f", c);
+
+            default:
+                if ((c > 0) && (b != 0)){
+                    printf("+%.1f", c);
+                }
+                else{
+                    printf("%.1f", c);
+                }
+        }
+        puts("=0\n");
+
+
         if ((a == 0) && (b == 0)) {
-            printf("Calculating: %.3f=0\n", c);
+            // printf("Calculating: %.3f=0\n", c);
             printf("Both a and b are 0, no real solutions.\n\n");
         }
         else if (a == 0) {
             // x = -c/b
-            printf("A is 0, calculating: -%.3f/%.3f = 0\n", c,b);
+            // printf("A is 0, calculating: -%.3f/%.3f = 0\n", c,b);
             float x = -c / b;
             printf("X = %.3f\n\n", x);
         }
         else {
-            printf("Calculating: %.3fx^2 + %.3fx + %.3f = 0\n", a,b,c);
+            // printf("Calculating: %.3fx^2 + %.3fx + %.3f = 0\n", a,b,c);
             // Check for discriminant:
             // b^2 - 4ac
             d = (pow(b, 2)) - (4 * a * c);
